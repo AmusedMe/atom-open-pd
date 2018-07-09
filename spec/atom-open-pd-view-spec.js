12590421c3c1cef0022d84e313a20ac9509504e2
Lewis <11472088@qq.com>
2018-07-09 14:30:44 +0800

    init

---
+++ spec/atom-open-pd-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomOpenPdView from '../lib/atom-open-pd-view';
+
+describe('AtomOpenPdView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
